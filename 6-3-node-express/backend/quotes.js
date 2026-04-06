a77893057f3e6867930bd50de0e31ab0e0bac06f
Fatimah <s202248340@kfupm.edu.sa>
2026-04-06 12:56:57 +0300

    t3

--- 6-3-node-express/backend/quotes.js
+++ 6-3-node-express/backend/quotes.js
@@ -1,5 +1,5 @@
 // Import random.js file
-
+import { getRandomQuote } from "./random.js";
 const quotes = [
   "Code is like humor. When you have to explain it, it’s bad.",
   "Before software can be reusable it first has to be usable.",
@@ -8,5 +8,6 @@ const quotes = [
 ];
 
 // TODO 3: Create and export getRandomQuote function
-//Hint: export function getRandomQuote(){}
-
+export function getRandomQuote(){
+  return quotes[Math.floor(Math.random() * quotes.length)];
+}
